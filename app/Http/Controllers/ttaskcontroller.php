422426f36333ba76cc39378b8487fdac45e3f324
Rutvik1103 <rutu054875@gmail.com>
2025-08-30 18:35:02 +0530

    update ttastcontroller

--- app/Http/Controllers/ttaskcontroller.php
+++ app/Http/Controllers/ttaskcontroller.php
@@ -18,17 +18,13 @@ class ttaskcontroller extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
+   
     public function show(string $id)
     {
         //
